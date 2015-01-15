1934d0d44bc8a127a8c44bd95da132bc79e3f42b
vikasvksingh <vikas.vksingh@gmail.com>
2015-01-14 17:42:57 -0800

    SharedVariable should be synchronized Properly

---
+++ Concurrency/src/com/snippets/SharedVariable.java
@@ -0,0 +1,32 @@
+package com.snippets;
+
+
+public class SharedVariable {
+
+	private static int sharedVariable = 0;
+	public static void main(String[] args) throws InterruptedException {
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				sharedVariable = 1;
+			}
+		}).start();
+		
+		for(int i=0;i<1000;i++) {
+			for(;;) {
+				if(sharedVariable == 1) {
+					break;
+				}
+			}
+		}
+		System.out.println("Value of SharedVariable : " + sharedVariable);
+	}
+
+}
