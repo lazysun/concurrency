9f7492dc56981aa782b6f404963e05312f5729da
vikasvksingh <vikas.vksingh@gmail.com>
2015-03-13 13:47:58 -0700

    REafctoring

--- Concurrency/src/com/snippets/StackReversal.java
+++ Concurrency/src/com/snippets/StackReversal.java
@@ -3,30 +3,18 @@ package com.snippets;
 import java.util.Stack;
 
 public class StackReversal {
-
-	Stack<Integer> stack = new  Stack<Integer>();
+	
 	public static void main(String[] args) {
 			StackReversal s = new StackReversal();
-			s.stack.push(1);
-			s.stack.push(2);
-			s.stack.push(3);
-			s.stack.push(4);
-			s.stack.push(5);
-			s.stack.push(6);
 			System.out.println(s.stack);
 			s.f();
 			System.out.println("After Reversing");
 			System.out.println(s.stack);
 			
 	}
-
 	void f() {
-		int size = stack.size();
-		int i=0;
-		while(i<size-1) {
-            int pop = stack.pop();
-			r(pop,i);
-			i++;
+		for(int i=0; i<stack.size()-1;i++) {
+			r(stack.pop(),i);
 		}
 	}
 
@@ -38,6 +26,15 @@ public class StackReversal {
 			 r(push, size);
 		 }
 		 stack.push(pop);
-		 return;
+	}
+	
+	Stack<Integer> stack = new  Stack<Integer>();
+	StackReversal() {
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		stack.push(4);
+		stack.push(5);
+		stack.push(6);
 	}
 }
